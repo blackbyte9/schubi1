1ebd30d57756f516d612a4662e499802c7df1a77
Black Byte <blackbyte9@gmx.net>
2025-05-29 08:20:41 +0200

    Add Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Schulbuch Bibliothek - Main</h1>
+        <pre>{JSON.stringify(session, null, 2)}  </pre>
       </main>
       <footer className="flex flex-col gap-4 items-center justify-center row-start-3">
         Schubi Vöhringen
---
+++ src/lib/auth/index.ts
@@ -0,0 +1,45 @@
+import NextAuth, { User, NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const BASE_PATH = "/api/auth";
+
+const authOptions: NextAuthConfig = {
+    providers: [
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials): Promise<User | null> {
+                const users = [
+                    {
+                        id: "admin1",
+                        userName: "admin",
+                        name: "Admin",
+                        password: "admin",
+                        email: "admin@donotreplay.com",
+                    },
+                ]
+
+                const user = users.find(
+                    (user) =>
+                        user.userName === credentials?.username &&
+                        user.password === credentials?.password
+                );
+
+                return user
+                    ? {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                    }
+                    : null;
+            }
+        }),
+    ],
+    basePath: BASE_PATH,
+    secret: process.env.NEXTAUTH_SECRET,
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
