213181a3b517a594fc77eff5c7e9cba1517417a7
Black Byte <blackbyte9@gmx.net>
2025-05-29 10:22:31 +0200

    Add protection and some test routes

---
+++ src/app/test-route/page.tsx
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import WhoAmIServerAction from "@/component/auth/WhoAmI.client";
+
+export default async function Page() {
+  const session = await auth();
+
+  async function onGetUserAction() {
+    "use server";
+    const session = await auth();
+    return session?.user?.name ?? null;
+  }
+
+  return (
+    <main>
+      <h1>Test Route</h1>
+      <div>User: {session?.user?.name}</div>
+      <WhoAmIServerAction onGetUserAction={onGetUserAction} />
+    </main>
+  );
+}
